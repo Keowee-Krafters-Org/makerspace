653c4f0018740be315018ab5e6be5ea2dfc567d0
Christopher Smith <christopher.smith@oopscope.com>
2025-07-29 13:14:41 -0400

    MS-010-Marketing-Feedback  - Updxated config to RELEASE-0.2.2

--- MembershipPortal/config.js
+++ MembershipPortal/config.js
@@ -14,7 +14,7 @@ config = {
 
   prod: {
     // Production configuration
-    version: 'RELEASE-0.2.2',
+    version: 'RELEASE-0.2.3',
     deploymentId: 'AKfycbywslFpBHt1OcsTyaE_gCRFrd3wjGhaOtbwr7mpO-hTMyyurJBM2tAiKX8cksRfmySR',
     imageFolderId: '1GBamyfVCCltpLC_rB70D4unHTcJQUFdY'
    }
